76e1d3817a932bbbcfaaa81a41ed843e6af25fe7
sandula-sanchana <sandulasanchana@gmail.com>
2025-10-08 11:50:34 +0530

    socket 2,scanner

--- src/main/java/edu/ijse/Client.java
+++ src/main/java/edu/ijse/Client.java
@@ -2,6 +2,7 @@ package edu.ijse;
 
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,7 +12,9 @@ public class Client {
             Socket remoteSocket=new Socket("localhost",2003);//ip,port
             System.out.println("Client started");
             DataOutputStream dataOutputStream=new DataOutputStream(remoteSocket.getOutputStream());
-            dataOutputStream.writeUTF("Hello Server, I am a Client");//write unicode string
+            Scanner scanner=new Scanner(System.in);
+            String msg=scanner.nextLine();
+            dataOutputStream.writeUTF("type ur msg: "+msg);//write unicode string
             dataOutputStream.flush();//save data in the stream
             remoteSocket.close();
         } catch (Exception e) {
